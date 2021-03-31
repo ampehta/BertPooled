6519dc175da90613eac70183cfe523b615186ad7
ampehta <68820979+ampehta@users.noreply.github.com>
2021-03-31 23:28:01 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@ def PoolBertDataset(X, y, model, tokenizer, config, mode = 'pool'): # mode = (po
     steps_per_epoch = math.ceil(len(X) / config.batch_size)
 
     emb = model.get_input_embeddings()
-    maxpool = tf.keras.layers.AveragePooling2D(pool_size=(2, 2), padding='valid')
+    average_pool = tf.keras.layers.AveragePooling2D(pool_size=(2, 2), padding='valid')
 
     embedded_X = []
     for n in range(iteration):
@@ -16,7 +16,7 @@ def PoolBertDataset(X, y, model, tokenizer, config, mode = 'pool'): # mode = (po
         X_ = tokenizer.batch_encode_plus(X_, truncation=True, padding=True, max_length=config.max_len,
                                          return_attention_mask=False, return_token_type_ids=False)['input_ids']
         if mode == 'pool': # dataset that pools the input embeddings of a bert
-            X_ = [tf.squeeze(maxpool(tf.reshape(sent, (1, -1, 768, 1)))) for sent in emb(X_)]
+            X_ = [tf.squeeze(average_pool(tf.reshape(sent, (1, -1, 768, 1)))) for sent in emb(X_)]
         elif mode =='basic.cls': # dataset that retrieves the cls token from a bert
             X_ = [model(tf.reshape(tf.constant(sent), (-1, 1)))['pooler_output'] for sent in X_]
         elif mode == 'basic_lstm': # dataset that retrieves input embeddings from a bert
