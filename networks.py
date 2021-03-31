16f82e2812266f562e886ba593c2e1fe0ac796b0
ampehta <68820979+ampehta@users.noreply.github.com>
2021-03-31 23:00:42 +0900

    feature: update networks.py

--- networks.py
+++ networks.py
@@ -2,15 +2,16 @@ import time
 from dataset import PoolBertDataset,data_loader
 from model import make_model
 
-def report_accuracy_and_time(train_X,train_y,test_X,test_y,model,tokenizer,config,mode):
+def report_accuracy_and_time(train_X,train_y,test_X,test_y,bert,tokenizer,config,mode):
     start_time = time.perf_counter()
-    dataset, steps_per_epoch = PoolBertDataset(train_X, train_y, model, tokenizer, config, mode)
+    dataset, steps_per_epoch = PoolBertDataset(train_X, train_y, bert, tokenizer, config, mode)
     dataloader = data_loader(dataset,config,train=True)
     model = make_model(config.learning_rate)
     model.fit(dataloader,epochs=config.epochs,steps_per_epoch=steps_per_epoch)
 
-    dataset_test, steps_per_epoch = PoolBertDataset(test_X, test_y, model, tokenizer, config, mode)
+    dataset_test, steps_per_epoch = PoolBertDataset(test_X, test_y, bert, tokenizer, config, mode)
     dataloader_test = data_loader(dataset_test, config, train=False)
     output = model.evaluate(dataloader_test,return_dict=True)
-    spent_time = start_time - time.perf_counter()
-    print(f"Time Spent: {spent_time}\nAccuracy :{output['accuracy']}")
+    spent_time = (time.perf_counter() - start_time)/60
+    print('##############################')
+    print(f"Time Spent: {spent_time}minutes\nAccuracy :{output['accuracy']}")
