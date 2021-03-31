cb925ef196347a7f8611797870657ceda01b21bd
ampehta <68820979+ampehta@users.noreply.github.com>
2021-03-31 21:18:31 +0900

    features: fixed reporting function

--- networks.py
+++ networks.py
@@ -1,5 +1,4 @@
 import time
-from configs import args
 from dataset import PoolBertDataset,data_loader
 from model import make_model
 
@@ -9,4 +8,9 @@ def report_accuracy_and_time(train_X,train_y,test_X,test_y,model,tokenizer,confi
     dataloader = data_loader(dataset,config,train=True)
     model = make_model(config.learning_rate)
     model.fit(dataloader,epochs=config.epochs,steps_per_epoch=steps_per_epoch)
-    model.predict(test_X,text_y)
+
+    dataset_test, steps_per_epoch = PoolBertDataset(test_X, test_y, model, tokenizer, config, mode)
+    dataloader_test = data_loader(dataset_test, config, train=False)
+    output = model.evaluate(dataloader_test,return_dict=True)
+    spent_time = start_time - time.perf_counter()
+    print(f"Time Spent: {spent_time}\nAccuracy :{output['accuracy']}")
